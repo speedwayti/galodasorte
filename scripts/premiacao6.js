a9d738e35e87bb0b9adf05f2964080483eb3900e
Wildyson Dantas <wildyson.santos@gmail.com>
2023-09-06 22:11:42 -0300

    Add files via upload

---
+++ scripts/premiacao6.js
@@ -0,0 +1,114 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+
+const arialFont = new FontFace(
+  "arial",
+  "url(./assets/arial-bold.ttf)"
+);
+arialFont.load().then(function (font) {
+  // with canvas, if this is ommited won't work
+  document.fonts.add(font);
+});
+
+const baseImage = new Image();
+baseImage.src = "./img/sorteiohojefloresta.jpg";
+
+
+document.getElementById("btnShare").addEventListener("click", shareImage);
+document.getElementById("btnSave").addEventListener("click", saveImage);
+
+
+async function shareImage() {
+  try {
+    gtag("event", "share");
+
+    canvas.toBlob((blob) => {
+      const filesArray = [
+        new File(
+          [blob],
+          document.querySelector("input").value.trim() + ".jpg",
+          {
+            type: "image/jpeg",
+            lastModified: new Date().getTime(),
+          }
+        ),
+      ];
+      const shareData = { files: filesArray };
+
+      navigator.share(shareData);
+    });
+  } catch (error) {
+    gtag("event", "exception", {
+      description: "[fn:shareImage] " + (error.message || error),
+      fatal: false,
+    });
+    Alert(
+      "Não foi possível compartilhar a imagem: " + (error.message || error)
+    );
+  }
+}
+
+function saveImage() {
+  try {
+    gtag("event", "download");
+
+    const a = document.createElement("a");
+    a.setAttribute("href", canvas.toDataURL("image/png"));
+    a.setAttribute("download", document.querySelector("input").value.trim());
+    a.click();
+  } catch (error) {
+    gtag("event", "exception", {
+      description: "[fn:saveImage] " + (error.message || error),
+      fatal: false,
+    });
+    Alert("Não foi possível baixar a imagem: " + (error.message || error));
+  }
+}
+
+$( "#btnSubmit" ).click(function() {
+  const n1 = $("#milhar").val();
+ // const n2 = $("#centena").val();
+  const data = $("#data").val();
+  
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(baseImage, 0, 0);
+
+  context.save();
+
+  context.textBaseline = "middle";
+  context.font = "100px arial";
+  context.fillStyle = "white";
+
+  context.fillText(data, 285, 705);
+
+  context.textBaseline = "middle";
+  context.font = "95px arial";
+  context.fillStyle = "white";
+ 
+  context.fillText("R$ " + n1, 282, 907);
+  //context.fillText(n2, 620, 943);
+  
+  context.restore();
+
+});
+
+$( "#btnClear" ).click(function() {
+  // const n1 = $( "#number1" ).val();
+  $("#milhar").val('');
+  //$("#centena").val('');
+  $("#data").val('');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(baseImage, 0, 0);
+  context.save();
+});
+
+
+window.onload = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(baseImage, 0, 0);
+
+  context.save();
+};
+
+
