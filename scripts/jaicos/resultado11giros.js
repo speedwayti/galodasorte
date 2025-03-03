cac16cf4fb4ab778349a0b684023e808e62cea9b
Wildyson Santos <wildyson.santos@gmail.com>
2025-03-03 17:14:54 -0300

    10 giros

---
+++ scripts/jaicos/resultado11giros.js
@@ -0,0 +1,183 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+const base = "../../";
+const city = getCity();
+
+const arialFont = new FontFace(
+  "arial",
+  "url("+base+"assets/artegra-sans-bold.otf)"
+);
+arialFont.load().then(function (font) {
+  // with canvas, if this is ommited won't work
+  document.fonts.add(font);
+});
+
+const baseImage = new Image();
+baseImage.src = city+"/resultado11giros.jpg";
+
+
+document.getElementById("btnShare").addEventListener("click", shareImage);
+document.getElementById("btnSave").addEventListener("click", saveImage);
+
+
+async function shareImage() {
+  try {
+    gtag("event", "share");
+
+    canvas.toBlob((blob) => {
+      const filesArray = [
+        new File(
+          [blob],
+          document.querySelector("input").value.trim() + ".jpg",
+          {
+            type: "image/jpeg",
+            lastModified: new Date().getTime(),
+          }
+        ),
+      ];
+      const shareData = { files: filesArray };
+
+      navigator.share(shareData);
+    });
+  } catch (error) {
+    gtag("event", "exception", {
+      description: "[fn:shareImage] " + (error.message || error),
+      fatal: false,
+    });
+    Alert(
+      "Não foi possível compartilhar a imagem: " + (error.message || error)
+    );
+  }
+}
+
+function saveImage() {
+  try {
+    gtag("event", "download");
+
+    const a = document.createElement("a");
+    a.setAttribute("href", canvas.toDataURL("image/png"));
+    a.setAttribute("download", document.querySelector("input").value.trim());
+    a.click();
+  } catch (error) {
+    gtag("event", "exception", {
+      description: "[fn:saveImage] " + (error.message || error),
+      fatal: false,
+    });
+    Alert("Não foi possível baixar a imagem: " + (error.message || error));
+  }
+}
+
+$( "#btnSubmit" ).click(function() {
+  const n1 = $("#number1").val();
+  const n2 = $("#number2").val();
+  const n3 = $("#number3").val();
+  const n4 = $("#number4").val();
+  const n5 = $("#number5").val();
+  const n6 = $("#number6").val();
+  const box1 = $("#box1").val();
+  const box2 = $("#box2").val();
+  const box3 = $("#box3").val();
+  const box4 = $("#box4").val();
+  const box5 = $("#box5").val();
+  const box6 = $("#box6").val();
+
+  const data = traduzData($("#data").val());
+  
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(baseImage, 0, 0);
+
+  context.save();
+
+  context.textBaseline = "middle";
+  context.textAlign = "center"
+  context.font = "90px arial";
+  context.fillStyle = "#8B0000";
+  context.fillStyle = "yellow";
+  context.fillText(data, 540, 320);
+
+  //context.textAlign ="left";
+  context.textBaseline = "middle";
+  context.font = "140px arial";
+  context.fillStyle = "white";
+  
+  context.fillText(n1, 560, 1040);
+
+  context.font = "130px arial";
+ 
+  
+
+ 
+ 
+  context.font = "45px arial";
+  context.fillText(box1, 570, 942);
+
+  context.font = "80px arial";
+  context.fillText(n2, 540, 1215);
+  context.fillText(n3, 180, 1355);
+  context.fillText(n4, 892, 1355);
+  context.fillText(n5, 540, 1493);
+  context.fillText(n5, 540, 1635);
+
+  context.font = "80px arial";
+
+  //ontext.fillText(segundoGiro, 560, 1270);
+ 
+  
+
+  context.fillText(box2, 180, 1215);
+  context.fillText(box3, 890, 1215);
+
+  context.fillText(box4, 535, 1355);
+  context.fillText(box5, 180, 1495);
+  context.fillText(box6, 895, 1495);
+
+
+  context.restore();
+
+});
+
+$( "#btnClear" ).click(function() {
+  // const n1 = $( "#number1" ).val();
+  $("#number1").val('');
+  $("#number2").val('');
+  $("#number3").val('');
+  $("#number4").val('');
+  $("#number5").val('');
+
+  $("#box1").val('');
+  $("#box2").val('');
+  $("#box3").val('');
+  $("#box4").val('');
+  $("#box5").val('');
+
+  $("#data").val('');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(baseImage, 0, 0);
+  context.save();
+});
+
+
+window.onload = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(baseImage, 0, 0);
+
+  context.save();
+};
+
+function traduzData(dataInf){
+  //const selectedDate = new Date(dataInf); // Converte o valor do input para uma data
+  const [year, month, day] = dataInf.split('-');
+  const selectedDate = new Date(year, month - 1, day);
+  const monthName = selectedDate.toLocaleString('pt-BR', { month: 'long' }); // Obtém o nome do mês em texto
+  return day +" DE " +monthName.toUpperCase();
+  //monthNameElement.textContent = `Mês selecionado: ${monthName}`;
+}
+
+function getCity(){
+  var path = window.location.pathname.split("/");
+  var tam = path.length;
+  return base+"img/"+path[tam-2];
+  
+}
+
+
